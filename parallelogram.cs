d1a30bc38118312903d6ae8c9d79130a019e7458
Alice3529 <71872976+Alice3529@users.noreply.github.com>
2023-04-02 11:13:25 +0300

    Update parallelogram.cs

--- parallelogram.cs
+++ parallelogram.cs
@@ -16,39 +16,14 @@ using System.Xml.Linq;
     public parallelogram(point a, point b, int length, screen screen1, string detailName=""):base(a, b, screen1, detailName)
     {
         length--;
-        CheckPointsOnErrors(a, b, screen1, detailName);
         this.length = length;
         addShape.Invoke(this);
     }
 
  
-    private void CheckPointsOnErrors(point a, point b, screen screen1, string detailName)
-    {
-        if (ClassError.CheckParameters(new point(a.x + 1, a.y + 1), b, detailName))
-        {
-            addInDrawList = false;
-        }
-        if (addInDrawList == true)
-        {
-            if (ClassError.CheckPoints(new point[] { a, b }, screen1, detailName))
-            {
-                addInDrawList = false;
-            }
-        }
-    }
-
     public override void draw()
     {
-        if (addInDrawList == false) { return; }
-
-        if (ClassError.CheckPoints(new point[] {nwest()}, screen1, detailName))
-        {
-            addInDrawList = false;
-        }
-
-        if (addInDrawList == false) { return; }
-
-
+       
         utility.put_line(swest(), seast(), screen1);
         utility.put_line(swest(), nwest(), screen1);
         utility.put_line(nwest(), neast(), screen1);
@@ -81,64 +56,40 @@ using System.Xml.Linq;
         ne.x += x;
         ne.y += y;
 
-        if (ClassError.CheckPoints(new point[] { sw, ne }, screen1, detailName, "перемещении"))
-        {
-            addInDrawList = false;
-        }
-
     }
 
     public void rotate_left()
     {
-        if (ClassError.CheckWasRotated(rotatable, detailName)) { return; }
         int diagonalX = Math.Abs(neast().x - swest().x);
         int diagonalY = Math.Abs(neast().y - swest().y);
         rotatable = true;
         ne = new point(swest().x, swest().y + length/2);
         sw = new point(swest().x-diagonalY*2, swest().y - diagonalX/2+length/2);
-
-        if (ClassError.CheckPoints(new point[] { sw, ne, nwest(), seast() }, screen1, detailName, "повороте"))
-        {
-            addInDrawList = false;
-        }
     }
 
     public void rotate_right()
     {
-        if (ClassError.CheckWasRotated(rotatable, detailName)) { return; }
         int diagonalX = Math.Abs(nwest().x - seast().x) / 2;
         int diagonalY = Math.Abs(nwest().y - seast().y) * 2;
         rotatable = true;
         sw = seast();
         ne = new point(sw.x + diagonalY, sw.y+diagonalX);
 
-        if (ClassError.CheckPoints(new point[] { sw, ne, nwest(), seast() }, screen1, detailName, "повороте"))
-        {
-            addInDrawList = false;
-        }
     }
 
     public void flip_horisontally()
     {
-        if (ClassError.CheckWasReflected(reflectable, detailName)) { return; }
         sw.x = seast().x - (seast().x - swest().x);
         ne.x = nwest().x;
         reflectable = true;
-        if (ClassError.CheckPoints(new point[] { sw, ne }, screen1, detailName, "отражении"))
-        {
-            addInDrawList = false;
-        }
+      
     }
 
     public void flip_vertically()
     {
-        if (ClassError.CheckWasReflected(reflectable, detailName)) { return; }
         ne.y -= 4* h;
         reflectable = true;
-        if (ClassError.CheckPoints(new point[] { sw, ne }, screen1, detailName, "отражении"))
-        {
-            addInDrawList = false;
-        }
+     
     }
 }
 
