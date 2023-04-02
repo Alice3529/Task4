37f7d6d52b7dc6fd17df379305bbc2b2bdec15e6
Alice3529 <71872976+Alice3529@users.noreply.github.com>
2023-04-02 11:15:14 +0300

    Update myshape.cs

--- myshape.cs
+++ myshape.cs
@@ -14,10 +14,6 @@ internal class myshape : rectangle
 
     public myshape(point a, point b, screen screen1, string detailName) : base(a, b, screen1, detailName)
     {
-        if (ClassError.CheckParameters(new point(a.x+4, a.y+3), b, detailName)) //размеры лица
-        {
-            addInDrawList = false;
-        }
         addShape.Invoke(this);
         ChangeFace();
     }
@@ -26,7 +22,6 @@ internal class myshape : rectangle
 
     public override void draw()
     {
-        if (addInDrawList == false) { return; }
         base.draw();
         int a = (swest().x + neast().x)/ 2;
         int b = (swest().y + neast().y) / 2;
@@ -35,7 +30,6 @@ internal class myshape : rectangle
 
     public override void move(int a, int b)
     {
-        if (addInDrawList == false) { return; }
         base.move(a, b);
         h = neast().y - swest().y + 1;
         w = neast().x - swest().x + 1;
@@ -47,15 +41,7 @@ internal class myshape : rectangle
 
     public override void resize(int size)
     {
-        if (addInDrawList == false) { return; }
         base.resize(size);
-        if (addInDrawList == false)
-        { 
-            l_eye.addInDrawList = false;
-            r_eye.addInDrawList=false;
-            mouth.addInDrawList=false;
-            return;
-        }
         l_eye.resize(size);
         r_eye.resize(size);
         mouth.resize(size);
@@ -64,7 +50,6 @@ internal class myshape : rectangle
 
     public void ChangeFace()
     {
-        if (addInDrawList == false) { return; }
         h = neast().y - swest().y + 1;
         w = neast().x - swest().x + 1;
         l_eye = new line(new point(swest().x + 2, swest().y + (h * 3 / 4)), 2, screen1, "левый глаз");
